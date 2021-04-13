773ceb857c5b9a922c35e679bdbb1642c67132db
sujanshrestha <shrestha94.sujan@gmail.com>
2021-04-13 11:21:42 -0500

    added functionality to use json server and fetch data, also added concept of conditional loading.
    Command: npx json-server --watch data/db.json --port 8000

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,14 @@ import Bloglist from './Bloglist'
 /*It's actually returning a as JSX Template not html,
 which is later changed into html*/
 function Home() {
-    const [blogs, setBlogs] = useState([
+    /*const [blogs, setBlogs] = useState([
         {title: 'Java', body: 'hello', author: 'John', id: 1},
         {title: 'Python', body: 'hello', author: 'Jane', id: 2},
         {title: 'React', body: 'hello', author: 'Johnny', id: 3},
-    ]);
+    ]);*/
+
+    /* Loading data from db.json */
+    const [blogs, setBlogs] = useState(null);
 
     function handleDelete(id) {
         const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -16,16 +19,26 @@ function Home() {
     }
 
     /* Hook: Runs every time there is a State change */
+    /*useEffect(() => {
+        console.log(blogs);
+        }, [blogs]);*/
+
     useEffect(() => {
-        console.log(blogs)
-;    }, [blogs]);
+       fetch('  http://localhost:8000/blogs')
+           .then(response => {
+               return response.json();
+           })
+           .then((data) => {
+               setBlogs(data);
+           })
+    });
 
     return(
         <div className="home">
             {/*Example of props*/}
-            <Bloglist blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
+            {blogs && <Bloglist blogs={blogs} title="All blogs!" handleDelete={handleDelete} /> }
             <hr />
-            <Bloglist blogs={blogs.filter((blog) => blog.title === 'Java')} title="Java blogs!" />
+           {/* <Bloglist blogs={blogs.filter((blog) => blog.author === 'mario')} title="mario blogs!" />*/}
         </div>
     )
 }
