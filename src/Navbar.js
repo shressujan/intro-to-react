68198b72aea70d01c9d46789562214bf83f535d6
sujanshrestha <shrestha94.sujan@gmail.com>
2021-04-13 11:51:39 -0500

    custom navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,12 +3,17 @@ import React from 'react'
 function Navbar() {
     return (
         <nav className="navbar">
+            <h1>The Shrestha Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/create" style={{
+                    color: 'white',
+                    backgroundColor: '#f1356d',
+                    borderRadius: '8px'
+                }}>New Blog</a>
             </div>
         </nav>
-    )
+    );
 
 
 }
