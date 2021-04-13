eca0d992386426e3a7a1bef839f802ec3f82ebb9
sujanshrestha <shrestha94.sujan@gmail.com>
2021-04-13 10:46:17 -0500

    added a navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+    )
+
+
+}
+
+
+export default Navbar;
